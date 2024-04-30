1664508f5e47c7f74cbffeed2ebf479bd1f2a7e9
YuvalBallas <99816808+YuvalBallas@users.noreply.github.com>
2024-04-30 19:59:34 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml up -d
+# Pull Docker images
+docker pull yuvalbal/super-app-node:latest
+docker pull yuvalbal/super-app-php:latest
+
+# Start containers
+docker-compose -f /path/to/your/docker-compose.yml up -d
+#docker compose -f docker-compose.yml up -d
